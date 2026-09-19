1fa7c23118c38efc554274c4fc932ec31de005ca
UzumakiNaruto3036 <ajiteshmenon69@gmail.com>
2026-09-19 10:56:07 +0400

    okkkk

---
+++ activity 1.py
@@ -0,0 +1,9 @@
+tree1=20
+tree2=25
+tree3=30
+tree4=35
+tree5=40
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of trees is:", sum)
+average=sum/5
+print("The average number of trees is:", average)    
